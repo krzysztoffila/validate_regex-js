3c72c090a526451c9475152eff7a4a0829d58b58
krzysztoffila <umowy.fila@gmail.com>
2023-03-21 12:44:14 +0100

    Init

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ * Opanuj JavaScript - Przeprogramowani.pl
+ * I. Fundamenty języka JavaScript
+ *
+ * Ćwiczenie 2 - "Walidacja"
+ */
+
+/*
+ * Cel zadania
+ *------------
+ * Zaimplementuj funkcję, która sprawdzi, czy przekazane hasło spełnia określone warunki:
+ *
+ * a) Ma długość od 3 do 10 znaków - OK
+ * b) Zawiera jeden ze znaków specjalnych - !, @ lub #
+ * c) Zawiera cyfrę
+ *
+ * Przykład:
+ * validatePassword('test') // => false
+ * validatePassword('test11!') // => true
+ */
+
+/*
+ * Punkty dodatkowe
+ *-----------------
+ * Funkcja powinna weryfikować, czy przekazany parametr jest typu string. Jeśli parametr nie
+ * spełnia tego warunku, funkcja powinna rzucić wyjątek.
+ */
+
+function validatePassword(password) {
+    if (typeof password === 'string') {
+        const re = /^(?=.*[0-9])(?=.*[!@#$])[a-zA-Z0-9!@#$]{3,10}$/;
+        return re.test(password.trim());
+    } else {
+        throw new Error('Invalid password');
+    }
+}
+/* Weryfikacja */
+
+function verify(input, goal) {
+    if (input === goal) {
+        console.log('Gratulacje!');
+    } else {
+        console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+}
+
+verify(validatePassword(''), false);
+verify(validatePassword('lol'), false);
+verify(validatePassword('ToDziala1#'), true);
